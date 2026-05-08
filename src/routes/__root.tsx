1efa7c5bafd7d313c7508bb55c6ec85efe12ba08
Studyforayush <studyfroayush@gmail.com>
2026-05-08 18:36:42 +0530

    chore: convert project to static Vercel SPA output

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,12 +4,10 @@ import {
   Link,
   createRootRouteWithContext,
   useRouter,
-  HeadContent,
-  Scripts,
 } from "@tanstack/react-router";
 
-import appCss from "../styles.css?url";
-import cursorEffectsCss from "../styles/cursor-effects.css?url";
+import "../styles.css";
+import "../styles/cursor-effects.css";
 import { CustomCursor } from "../components/CustomCursor";
 
 function NotFoundComponent() {
@@ -101,17 +99,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
 });
 
 function RootShell({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <HeadContent />
-      </head>
-      <body>
-        {children}
-        <Scripts />
-      </body>
-    </html>
-  );
+  return <>{children}</>;
 }
 
 function RootComponent() {
